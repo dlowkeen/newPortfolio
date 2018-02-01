9582eb1a90a9487b076fc85caa6aa7c0a9f194b7
Donovan Lowkeen <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-01-31 16:48:03 -0800

    added photography header

--- newportfolio/src/components/body/photography/PhotographyPage.js
+++ newportfolio/src/components/body/photography/PhotographyPage.js
@@ -4,6 +4,7 @@ import HeaderPhoto from "../../common/HeaderPhoto";
 import Landing from "../landing/Landing";
 import Photography from "./Photography";
 import Footer from "../../common/Footer";
+import SectionHead from '../../common/SectionHead';
 require("../body.css");
 
 class PhotographyPage extends Component {
@@ -13,6 +14,7 @@ class PhotographyPage extends Component {
         <HeaderPhoto />
         <Landing />
         <Container>
+          <SectionHead name="Photography" />
           <Photography />
         </Container>
         <Footer />
