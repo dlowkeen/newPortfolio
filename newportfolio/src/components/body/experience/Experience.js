96db029806315b97102c756448e5fede05edf912
Donovan <donovanlowkeen@Donovans-MacBook-Pro.local>
2018-05-24 18:31:09 -0700

    added some work experience

--- newportfolio/src/components/body/experience/Experience.js
+++ newportfolio/src/components/body/experience/Experience.js
@@ -1,13 +1,78 @@
 import React, { Component } from 'react';
+import { Item, Label, Icon, Button, Container } from 'semantic-ui-react';
 import SectionHead from '../../common/SectionHead';
+import rhiz from "../../../assets/images/rhiz.png";
+
+const paragraph = "Duis mollit irure enim proident aliquip laboris cillum et excepteur voluptate proident."
 
 class Experience extends Component {
   render() {
-    return (
-      <div>
-        <SectionHead name="Experience"/>
-      </div>
-    );
+    return <Container>
+        <SectionHead name="Experience" />
+        <Item.Group divided>
+          <Item>
+            <Item.Image as="a" href="https://github.com/dlowkeen/rhiz" size="medium" src={rhiz} />
+
+            <Item.Content>
+              <Item.Header style={{ color: "white" }} as="a" href="https://github.com/dlowkeen/rhiz">
+                Rhiz
+              </Item.Header>
+              <Item.Meta style={{ color: "white" }}>
+                <span>Software Engineer</span>
+              </Item.Meta>
+              <Item.Description style={{ color: "white" }}>
+                <p>
+                  Small business marketplace to help users find businesses
+                  using Localeze API and in-network customer reviews.
+                </p>
+                <p>
+                  Contribute to an agile, collaborative environment using
+                  CircleCI and Jasmine Unit Tests for automated continuous
+                  integration and deployment with GitHub and Heroku.
+                </p>
+              </Item.Description>
+              <Item.Extra>
+                <Label>React/Redux</Label>
+                <Label>Node/Express</Label>
+                <Label>MongoDB</Label>
+                <Label>CircleCI</Label>
+                <Label>Jasmine Unit Testing</Label>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+
+          <Item>
+            <Item.Image as="a" href="https://github.com/dlowkeen/rhiz" size="medium" src={rhiz} />
+
+            <Item.Content>
+              <Item.Header style={{ color: "white" }} as="a" href="https://github.com/dlowkeen/rhiz">
+                Social Spaces
+              </Item.Header>
+              <Item.Meta style={{ color: "white" }}>
+                <span>Front-End Developer</span>
+              </Item.Meta>
+              <Item.Description style={{ color: "white" }}>
+                <p>
+                  Small business marketplace to help users find businesses
+                  using Localeze API and in-network customer reviews.
+                </p>
+                <p>
+                  Contribute to an agile, collaborative environment using
+                  CircleCI and Jasmine Unit Tests for automated continuous
+                  integration and deployment with GitHub and Heroku.
+                </p>
+              </Item.Description>
+              <Item.Extra>
+                <Label>React/Redux</Label>
+                <Label>Node/Express</Label>
+                <Label>MongoDB</Label>
+                <Label>CircleCI</Label>
+                <Label>Jasmine Unit Testing</Label>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Container>;
   }
 }
 
